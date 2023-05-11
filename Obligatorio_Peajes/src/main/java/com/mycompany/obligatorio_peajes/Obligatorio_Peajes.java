cbf21d97341456b99f90647b6a92963e5b0d4fb8
SebastianPaulos <91295477+SebastianPaulos@users.noreply.github.com>
2023-05-10 22:10:16 -0300

    Add files via upload

---
+++ Obligatorio_Peajes/src/main/java/com/mycompany/obligatorio_peajes/Obligatorio_Peajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.obligatorio_peajes;
+
+/**
+ *
+ * @author Digital
+ */
+public class Obligatorio_Peajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
