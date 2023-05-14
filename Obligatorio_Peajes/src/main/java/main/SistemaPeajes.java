14c4e92612127f06b7c6a7a518f51d3e6cbd77d1
SebastianPaulos-ORT <SP159689@fi365.ort.edu.uy>
2023-05-14 19:19:31 -0300

    Dialogo LoginPropietario

---
+++ Obligatorio_Peajes/src/main/java/main/SistemaPeajes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import iuGrafica.InicioAplicacion;
+
+/**
+ *
+ * @author Digital
+ */
+public class SistemaPeajes {
+
+    public static void main(String[] args) {
+       InicioAplicacion sistema = new InicioAplicacion();
+       sistema.setVisible(true);
+    }
+}
