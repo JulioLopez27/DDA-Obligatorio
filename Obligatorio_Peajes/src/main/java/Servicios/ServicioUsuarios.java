1ff543fbf2ee2d1b898a830c0ea3d8cb8b4f92b1
Julio Lopez <julioneo95@hotmail.com>
2023-05-17 19:46:13 -0300

    feature: add logica login

--- Obligatorio_Peajes/src/main/java/Servicios/ServicioUsuarios.java
+++ Obligatorio_Peajes/src/main/java/Servicios/ServicioUsuarios.java
@@ -5,6 +5,7 @@
 package Servicios;
 
 import dominio.Sesion;
+import dominio.Usuario;
 import dominio.UsuarioAdministrador;
 import dominio.UsuarioPropietario;
 import java.util.ArrayList;
@@ -15,24 +16,47 @@ import java.util.List;
  * @author Julio Cesar
  */
 public class ServicioUsuarios {
-    
-    private List<UsuarioPropietario>usuariosPropietario;
+
+    private List<UsuarioPropietario> usuariosPropietario;
     private List<UsuarioAdministrador> usuariosAdministrador;
     private List<Sesion> sesiones;
 
     public ServicioUsuarios() {
-        usuariosPropietario= new ArrayList<>();
-        usuariosAdministrador= new ArrayList<>();
-        sesiones= new ArrayList<>();
-    }
-    
-  
-    Sesion loginUsuarioPropietario(String cedula, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        usuariosPropietario = new ArrayList<>();
+        usuariosAdministrador = new ArrayList<>();
+        sesiones = new ArrayList<>();
     }
 
-    UsuarioAdministrador loginUsuarioAdministrador(String cedula, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public Sesion loginUsuarioPropietario(String cedula, String password) {
+       UsuarioPropietario usuarioLogueado=(UsuarioPropietario)this.loginUsuario(cedula, password, (ArrayList)usuariosPropietario);
+       if(usuarioLogueado!=null){
+           Sesion sesion= new Sesion(usuarioLogueado);
+           this.agregar(sesion);
+           return sesion;
+       }
+       return null;
     
+    }
+
+    public UsuarioAdministrador loginUsuarioAdministrador(String cedula, String password) {
+       return (UsuarioAdministrador)this.loginUsuario(cedula, password,(ArrayList)usuariosAdministrador);
+    }
+
+    private Usuario loginUsuario(String cedula, String password, List<Usuario> usuarios) {
+        for (Usuario u : usuarios) {
+            int cedulaNro = Integer.parseInt(cedula);
+            if (u.getCedula() == cedulaNro && u.esPasswordValida(password)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    
+    
+    
+    
+    private void agregar(Sesion sesion) {
+        sesiones.add(sesion);
+    }
 }
