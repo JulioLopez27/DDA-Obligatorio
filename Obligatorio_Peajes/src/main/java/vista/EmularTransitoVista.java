6e05943af07cf86a703f8ec1750312b20b7f21af
Julio Lopez <julioneo95@hotmail.com>
2023-06-03 19:47:45 -0300

    Creacion Transito vista

---
+++ Obligatorio_Peajes/src/main/java/vista/EmularTransitoVista.java
@@ -0,0 +1,19 @@
+
+package vista;
+
+import java.util.List;
+import modelo.Puesto;
+import modelo.Tarifa;
+import modelo.Transito;
+
+/**
+ *
+ * @author Julio Cesar
+ */
+public interface EmularTransitoVista {
+    public void cargarComboPuestos(List<Puesto> puestos);
+    public void mostrarDatosDeTransito(String detallesTransito);
+    public void mostrarMensaje(String mensaje);
+    public void mostrarTarifasDePuesto(List<Tarifa> tarifas);
+
+}
